4030eb36f6223f99764378db571e79c3cec70439
leidy731 <leidyupta@gmail.com>
2026-01-30 08:03:25 -0400

    feat: add initial Next.js configuration for development, build error handling, and IPFS export.

--- packages/nextjs/next.config.ts
+++ packages/nextjs/next.config.ts
@@ -4,11 +4,11 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   devIndicators: false,
   typescript: {
-    // ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    // ignoreBuildErrors: true,
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
